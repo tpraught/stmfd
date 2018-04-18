14018a9440b61463216bc3e89a7e5317863ccd4e
Julia Lidstrom <juleslidstrom@gmail.com>
2018-04-18 12:23:10 -0500

    Added MVP pages

---
+++ client/src/pages/Explorers/index.js
@@ -0,0 +1,4 @@
+export { default } from "./Explorer_About";
+export { default } from "./Explorer_Events";
+export { default } from "./Explorer_Roster";
+export { default } from "./Explorer_Schedule";
