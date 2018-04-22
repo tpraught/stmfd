4beb37e0f6f8ea07b9bf5dfc720c4522ff71d776
Jeff DuBois <jeff.adubois@gmail.com>
2018-04-22 08:03:11 -0500

    Added Admin model and seed file

---
+++ scripts/seedAdmin.js
@@ -0,0 +1,34 @@
+///Connection with the DB and seeding with data
+//MODELS NAMES NEED TO BE UPDATED
+
+const mongoose = require("mongoose");
+const db = require("../models");
+mongoose.Promise = global.Promise;
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/firedepartment");
+
+const admin = [
+   {
+	  email: "admin@gmail.com",
+	   password: "admin",
+   },
+  ];
+
+//remove all records from Roster, then insert the 'seed' records from above
+db.Admin
+  .remove({})
+  .then(() => db.Admin.collection.insertMany(roster))
+  .then(data => {
+	console.log("Admin data is \n" + JSON.stringify(data));
+    console.log(data.ops.length + " records inserted!");
+	process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+ 
+ 
