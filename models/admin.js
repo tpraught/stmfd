763a4cc0e19fd748aaecdd93ee40c39b34ae1600
kvb <kumarivb@gmail.com>
2018-04-25 12:05:15 -0500

    updated server.js with session and mongoStore

--- models/admin.js
+++ models/admin.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 var bcrypt = require('bcrypt');
+
 const Schema = mongoose.Schema;
 
 const adminSchema = new Schema({
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
@@ -15,6 +19,36 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/firedepartment");
+var db = mongoose.connection;
+
+// Handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
+
+// Use sessions for tracking logins
+app.use(session({
+    secret: 'web rescue',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection:db
+    })
+}));
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+// Error handler
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+  });
 
 // Start the API server
 app.listen(PORT, function() {
