fc7aa25cdc00daae95b258c7bc994d1282a8d27a
Julia Lidstrom <juleslidstrom@gmail.com>
2018-04-27 20:18:24 -0500

    Fixing a bug

--- client/src/pages/07_Admin_Roster/AdminRoster.js
+++ client/src/pages/07_Admin_Roster/AdminRoster.js
@@ -40,8 +40,7 @@ class FireRoster extends Component {
       this.setState({
         editing: false,
       })
-   //set this.state.editig = false
-    //reload the page  this.loadRoster()
+
     API.editRecord({
       id: newFireFighter.id,
       first_name: newFireFighter.firstName,
@@ -100,9 +99,6 @@ class FireRoster extends Component {
         <Row>
           <Col size="md-9">
 
-
-      
-
           <EditModal
             isOpen = {this.state.editing}
             onSave = {(newFireFighter) => {
@@ -116,7 +112,7 @@ class FireRoster extends Component {
            memberSince = {this.state.memberSince}
            title = {this.state.title}
            rank = {this.state.rank}
-           station = {this.state.station} //drop-down values are not populating the fields
+           station = {this.state.station} 
            company = {this.state.company}
             onCancel = {() => {
               this.setState({
