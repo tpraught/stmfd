b91d4367f526e22428f2aeb280d0818b29198888
Praught <tpraught@osii.com>
2018-04-29 07:43:47 -0500

    styling the admin pages

--- client/src/pages/07_Admin_Roster/AdminRoster.js
+++ client/src/pages/07_Admin_Roster/AdminRoster.js
@@ -2,15 +2,9 @@ import React, { Component } from "react";
 import { Table, Button } from 'reactstrap';
 import { Link } from "react-router-dom";
 import AdminRosterTable from "../../components/AdminRoster";
-
 import AdminHeader from "../../components/AdminHeader";
-
-
 import EditModal from "../../components/Modal";
-
-// import Button from "../../components/Button";
 import {  Col, Row, Container } from "../../components/Grid";
-
 import API from "../../utils/API";
 
 class FireRoster extends Component { 
@@ -54,7 +48,6 @@ class FireRoster extends Component {
     })
    
     this.loadRoster();
-    
   }
   
   loadRoster = () => {
@@ -90,84 +83,84 @@ class FireRoster extends Component {
   }
    
  
-    render() {
+  render() {
 
     return (
-     <div> 
-      <AdminHeader/>
-      <Container fluid>
-        <Row>
-          <Col size="md-9">
+      <div> 
+        <AdminHeader/>
+        <Row className="justify-content-center">
+          <Container fluid>
+            <Col size="md-9">
 
-          <EditModal
-            isOpen = {this.state.editing}
-            onSave = {(newFireFighter) => {this.saveRecord(newFireFighter);
-            }}
-           id = {this.state.id}
-           firstName = {this.state.firstName}
-           lastName = {this.state.lastName}
-           fireNumber = {this.state.fireNumber}
-           memberSince = {this.state.memberSince}
-           title = {this.state.title}
-           rank = {this.state.rank}
-           station = {this.state.station} 
-           company = {this.state.company}
-            onCancel = {() => {
-              this.setState({
-                editing: false
-              })
-            }} />
-            <Row>
-              <Col size="md-9"> 
-              <Link to="/admin/add"> 
-                <Button outline color="danger">ADD FIREFIGHTER 
-                </Button>
-               </Link>
-               
-               </Col> 
-           </Row>
+            <EditModal
+              isOpen = {this.state.editing}
+              onSave = {(newFireFighter) => {this.saveRecord(newFireFighter);
+              }}
+            id = {this.state.id}
+            firstName = {this.state.firstName}
+            lastName = {this.state.lastName}
+            fireNumber = {this.state.fireNumber}
+            memberSince = {this.state.memberSince}
+            title = {this.state.title}
+            rank = {this.state.rank}
+            station = {this.state.station} 
+            company = {this.state.company}
+              onCancel = {() => {
+                this.setState({
+                  editing: false
+                })
+              }} />
+              <Row>
+                <Col size="md-9"> 
+                <Link to="/admin/add"> 
+                  <Button outline color="danger">ADD FIREFIGHTER 
+                  </Button>
+                </Link>
+                
+                </Col> 
+            </Row>
 
-      <Table className = "rosterTable">  
-       <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Fire #</th>
-            <th>Year</th>
-            <th>Title</th>
-            <th>Rank</th>
-            <th>Station</th>
-            <th>Company</th>
-            <th>Admin</th>
-          </tr>
-        </thead>
-       
-        <tbody>
-        {this.state.fireFighters.map(fireFighter =>( 
-          
-            <AdminRosterTable
-            key={fireFighter._id} 
-            id={fireFighter._id} 
-            firstName= {fireFighter.first_name}
-            lastName = {fireFighter.last_name}  
-            fireNumber={fireFighter.fire_number}   
-            year={fireFighter.year_started}  
-            title={fireFighter.title}  
-            rank={fireFighter.rank}  
-            station={fireFighter.station}  
-            company={fireFighter.company}
-            onEdit={() => this.editRecord(fireFighter)}
-            onDelete={() => this.deleteRecord(fireFighter._id)}
-            >
-            </AdminRosterTable>  
-            
+        <Table className = "rosterTable">  
+        <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Fire #</th>
+              <th>Year</th>
+              <th>Title</th>
+              <th>Rank</th>
+              <th>Station</th>
+              <th>Company</th>
+              <th>Admin</th>
+            </tr>
+          </thead>
         
-        ))}
-        </tbody>
-      </Table>
-      </Col>
+          <tbody>
+          {this.state.fireFighters.map(fireFighter =>( 
+            
+              <AdminRosterTable
+              key={fireFighter._id} 
+              id={fireFighter._id} 
+              firstName= {fireFighter.first_name}
+              lastName = {fireFighter.last_name}  
+              fireNumber={fireFighter.fire_number}   
+              year={fireFighter.year_started}  
+              title={fireFighter.title}  
+              rank={fireFighter.rank}  
+              station={fireFighter.station}  
+              company={fireFighter.company}
+              onEdit={() => this.editRecord(fireFighter)}
+              onDelete={() => this.deleteRecord(fireFighter._id)}
+              >
+              </AdminRosterTable>  
+              
+          
+          ))}
+          </tbody>
+        </Table>
+        </Col>
+        </Container>
         </Row>
-      </Container>
 
       </div>
     )
