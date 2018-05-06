8e15e866d46a75751c802b01a6613a1f535db2fb
kvb <kumarivb@gmail.com>
2018-05-06 13:29:21 -0500

    passport

--- models/admin.js
+++ models/admin.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // var bcrypt = require('bcrypt');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const User = new Schema({
     username: String,
-    password: String,
     firstName: String,
     lastName: String
 });
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User, User');
 
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
@@ -18,6 +19,14 @@ app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 
+// ----------------------------- admin ----------------------------------------------
+// Configure Passport
+const User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// ----------------------------- ----- ----------------------------------------------
+
 var MONGODB_URI =process.env.MONGODB_URI || "mongodb://localhost/firedepartment"
   mongoose.Promise = global.Promise;
   mongoose.connect(MONGODB_URI,function(error){
@@ -28,8 +37,19 @@ var MONGODB_URI =process.env.MONGODB_URI || "mongodb://localhost/firedepartment"
      }
   });
 
+// ----------------------------- admin ----------------------------------------------
 // connect mongoose
 mongoose.connect('mongodb://localhost/userlist');
+
+app.use(require('express-session')({
+    secret: 'stop the fire',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+// ----------------------------- ----- ----------------------------------------------
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
