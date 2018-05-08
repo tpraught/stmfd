159c0dfd8d514ae353516344db6add6004416c6e
Praught <tpraught@osii.com>
2018-05-07 19:43:42 -0500

    updates the schedule table - front-end

--- client/src/components/Events/EventsTable.js
+++ client/src/components/Events/EventsTable.js
@@ -4,11 +4,12 @@ const EventsTable = props => (
 	<tr key = {props.id}>
 		<td width="5%" className="text-center">{props.date}</td>
 		<td width="35%">{props.description}</td>
-		<td width="5%" className="text-center">{props.officerStartTime}</td>
-		<td width="5%" className="text-center">{props.meetingStartTime}</td>
-		<td width="5%" className="text-center">{props.trainingStartTime}</td>
-		<td width="5%" className="text-center">{props.drillCode}</td>
-		<td width="20%" className="text-center">{props.lunchCommittee}</td>
+		<td width="7%" className="text-center">{props.officerStartTime}</td>
+		<td width="7%" className="text-center">{props.meetingStartTime}</td>
+		<td width="7%" className="text-center">{props.trainingStartTime}</td>
+		<td width="7%" className="text-center">{props.drillCode}</td>
+		<td width="14%" className="text-center">{props.standard}</td>
+		<td width="18%" className="text-center">{props.lunchCommittee}</td>
   </tr>
     );
 
