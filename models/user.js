5dac36f34f9a46b2269b34ec2c4df9059c9484f3
Tonye Praught <31660406+tpraught@users.noreply.github.com>
2018-05-09 18:29:05 -0500

    Revert "notes"

--- models/user.js
+++ models/user.js
@@ -1,46 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+const User = new Schema({
+    email: String,
+    firstName: String,
+    lastName: String,
+    passwordReset: { type: String, select: false },
+    username: String
 });
 
-UserSchema.pre('save', function(next) {
-    var user = this;
-    if (this.isModified('password') || this.isNew) {
-        bcrypt.genSalt(10, function(err, salt) {
-            if (err) {
-                return next(err);
-            }
-            bcrypt.hash(user.password, salt, null, function(err, hash) {
-                if (err) {
-                    return next(err);
-                }
-                user.password = hash;
-                next();
-            });
-        });
-    } else {
-        return next();
-    }
-});
+User.plugin(passportLocalMongoose);
 
-UserSchema.methods.comparePassword = function(passw, cb) {
-    bcrypt.compare(passw, this.password, function (err, isMatch) {
-        if (err) {
-            return cb(err);
-        }
-        cb(null, isMatch);
-    });
-};
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', User);
